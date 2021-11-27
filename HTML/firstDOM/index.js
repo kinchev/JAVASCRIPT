3d6f73e382cc227a252f1202c1cf5f7cdbb07ed8
Kristian Kinchev <83530894+kinchev@users.noreply.github.com>
2021-11-27 23:08:47 +0200

    firstDOM

---
+++ HTML/firstDOM/index.js
@@ -0,0 +1,9 @@
+const changeElement = () => {
+    header.style.backgroundColor = "red";
+    header.style.color = "green";
+};
+const resetElement = () => {
+    const header = document.getElementById("header");
+    header.style.backgroundColor = "white";
+    header.style.color = "black";
+};

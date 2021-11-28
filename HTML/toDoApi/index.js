2dcc88d62eb1395fea0ea026f557a5a2f9755494
Kristian Kinchev <83530894+kinchev@users.noreply.github.com>
2021-11-28 10:18:10 +0200

    todoApi

---
+++ HTML/toDoApi/index.js
@@ -0,0 +1,16 @@
+const addToDo = () => {
+    const input = document.getElementById("texr-input");
+    const list = document.getElementsById("todos-list");
+    const todo = document.createElement("li");
+
+    todo.innerText = input.value;
+    //add ttoddo to the list
+    list.appendChild(todo);
+    //Clear input
+    input.value = "";
+};
+(()=>{
+    const btn=documnet.getElementById('btn');
+}
+
+)
